e4c7aa904f843ae8fa9a3b6be1773f22a2b995f8
Vadlox <ramsesplayzytx@gmail.com>
2025-05-17 20:04:21 -0500

    Commit. Added Version Checker.

--- src/main/java/vadlox/dev/hydraJoinMsgs/HydraJoinMsgs.java
+++ src/main/java/vadlox/dev/hydraJoinMsgs/HydraJoinMsgs.java
@@ -10,12 +10,19 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import java.time.Year;
 import java.util.Collections;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import me.clip.placeholderapi.PlaceholderAPI;
 
 public class HydraJoinMsgs extends JavaPlugin implements Listener, TabCompleter {
+    private static final String REPO_URL = "https://api.github.com/repos/Vadlox/HydraJoinMsgs/releases/latest";
+
     @Override
     public void onEnable() {
         // Save default config on first run
@@ -31,6 +38,7 @@ public class HydraJoinMsgs extends JavaPlugin implements Listener, TabCompleter 
                 "  ██║  ██║██║       Portfolio: https://vadlox.hydraquest.net\n" +
                 "  ╚═╝  ╚═╝╚═╝\n";
         getLogger().info(enableMessage);
+        checkForUpdates();
     }
 
     @Override
@@ -43,6 +51,39 @@ public class HydraJoinMsgs extends JavaPlugin implements Listener, TabCompleter 
                 "  ██║  ██║██║       Portfolio: https://vadlox.hydraquest.net\n" +
                 "  ╚═╝  ╚═╝╚═╝\n";
         getLogger().info(disableMessage);
+    }
+
+    private void checkForUpdates() {
+        String currentVersion = getDescription().getVersion();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(REPO_URL);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    String latestVersion = response.toString().split("\"tag_name\":\"")[1].split("\"")[0];
+                    if (!currentVersion.equals(latestVersion)) {
+                        for (Player player : getServer().getOnlinePlayers()) {
+                            if (player.hasPermission("hydrajoinmsgs.admin")) {
+                                player.sendMessage(ChatColor.YELLOW + "[HydraJoinMsgs] A new version (" + latestVersion + ") is available! Download it at: https://github.com/Vadlox/HydraJoinMsgs/releases");
+                            }
+                        }
+                        getLogger().info(ChatColor.YELLOW + "A new version (" + latestVersion + ") is available! Download it at: https://github.com/Vadlox/HydraJoinMsgs/releases");
+                    }
+                } catch (Exception e) {
+                    getLogger().warning(ChatColor.RED + "Failed to check for updates: " + e.getMessage());
+                }
+            }
+        }.runTaskAsynchronously(this);
     }
 
     @Override
